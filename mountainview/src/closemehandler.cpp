a8ab5899227f9aa66e22d99a43493d8110ab8ddd
Jeremy Magland <magland@jm02.scdanet.org>
2016-04-26 06:54:58 -0400

    add debug code to closemehandler

--- mountainview/src/closemehandler.cpp
+++ mountainview/src/closemehandler.cpp
@@ -38,9 +38,11 @@ void CloseMeHandler::start()
 
 void CloseMeHandler::slot_timer()
 {
+    qDebug() << "DEBUG" << __FUNCTION__ << __FILE__ << __LINE__ << "CLOSEME 1";
     QString fname = qApp->applicationDirPath() + "/closeme.tmp";
     if (QFile::exists(fname)) {
         QDateTime dt = QFileInfo(fname).created();
+        qDebug() << "DEBUG" << __FUNCTION__ << __FILE__ << __LINE__ << "CLOSEME FILE EXISTS, CREATED AT" << dt.toString("MM-dd-yyyy::hh-mm-ss-zzz") << "START TIME IS" << d->m_start_time.toString("MM-dd-yyyy::hh-mm-ss-zzz");
         if (dt > d->m_start_time) {
             exit(0);
         }
